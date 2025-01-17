35d9a5c7bbcfb7371b3d12ca8dd9f3f6e5309a97
Alex <alexandre.kocev@gmail.com>
2025-01-18 00:20:28 +0700

    update the redirect_uri

--- main.py
+++ main.py
@@ -112,10 +112,10 @@ authenticator = Authenticate(
     secret_credentials_path = 'temp_google_credentials.json',
     cookie_name='cookie_name',
     cookie_key='cookie_key',
-    # redirect_uri = 'http://buddytools.scandiweb.com/'
+    redirect_uri = 'http://buddytools.scandiweb.com/'
     # redirect_uri = 'http://127.0.0.1:8080/',
     # redirect_uri = 'http://localhost:8501/',
-    redirect_uri = 'https://scandi-buddy-578201479770.europe-west1.run.app',
+    # redirect_uri = 'https://scandi-buddy-578201479770.europe-west1.run.app',
 
     
 )
